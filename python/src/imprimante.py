12494565047917e11c08a778d3ff5feb9de84ffa
deboc <pierre@debeaucorps.com>
2013-05-30 04:15:46 +0200

    étalonnages et protocoles ok pour les 2 déplacements.

--- python/src/imprimante.py
+++ python/src/imprimante.py
@@ -10,6 +10,7 @@ class Imprimante:
     
     baudrate = 9600
     ping = '#'
+    newLine = "\n"
     
     def __init__(self):
         
@@ -19,39 +20,53 @@ class Imprimante:
         #les threads attendent la serie
         self.prete = False
         
+        #recherche du port série
+        self.attribuer()
+        
     def attribuer(self):
         """
         Cette méthode invoquée au lancement du service série permet de détecter et stocker le chemin pour communiquer avec la carte.
         """
-        #liste les chemins trouvés dans /dev
-        sources = os.popen('ls -1 /dev/ttyUSB* 2> /dev/null').readlines()
-        sources.extend(os.popen('ls -1 /dev/ttyACM* 2> /dev/null').readlines())
         
-        for k in range(len(sources)):
-            sources[k] = sources[k].replace("\n","")
-        
-        k=0
-        while k < len(sources):
+        #liste les périphériques présents sur les ports COM
+        sources = []
+        for location in ["com"+str(i) for i in range(22)]:
             try:
-                instanceSerie = Serial(sources[k], Imprimante.baudrate, timeout=0.1)
+                serialport = Serial(location, 9600, timeout = 0)
+                sources.append(location)
+                serialport.close()
+            except Exception as e:
+                #aucun périphérique
+                pass
+                
+        #périphériques usb : en général en fin de liste, donc on gagne du temps
+        sources.reverse()
+        
+        for source in sources:
+            try:
+                instanceSerie = Serial(source, Imprimante.baudrate, timeout=0.1)
                 
                 #vide le buffer série coté pc
                 instanceSerie.flushInput()
                 
-                #vide le buffer de l'avr
-                instanceSerie.write(bytes(" \r","utf-8"))
+                #initialisation de l'arduino (quit recoit un reset à l'instanciation de la série)
+                time.sleep(2)
+                
+                #évacuation du message de fin d'initialisation (gardé pour le debug)
+                instanceSerie.readline()
+        
+                #envoi d'une demande d'identifiant (ping)
+                instanceSerie.write(bytes("?"+Imprimante.newLine,"utf-8"))
+                
+                #évacuation de la trame d'acquittement
                 instanceSerie.readline()
                 
-                #envoi d'une demande d'identifiant (ping)
-                instanceSerie.write(bytes("?\r","utf-8"))
-                #évacuation de l'acquittement
-                instanceSerie.readline()
                 #réception de l'id de la carte
                 rep = self._clean_string(str(instanceSerie.readline(),"utf-8"))
                 
                 #lecture de l'identifiant
                 if rep == Imprimante.ping:
-                    print("imprimante trouvée sur "+sources[k])
+                    print("imprimante trouvée sur "+source)
                     self.serie = instanceSerie
                     break
                 else:
@@ -59,11 +74,9 @@ class Imprimante:
                     instanceSerie.close()
             except Exception as e:
                 print("exception durant la détection des périphériques série: {0}".format(e))
-            k += 1
                     
         if not self.serie:
-            print("imprimante non trouvée !")
-        time.sleep(1)
+            raise Exception("Imprimante non trouvée ! Est-elle bien branchée ?")
         self.prete = True
         
     def _clean_string(self, chaine):
@@ -89,9 +102,9 @@ class Imprimante:
         for message in messages:
             #print("message : >"+str(message)+"<")#DEBUG
             try:
-                self.serie.write(bytes(str(message) + '\r',"utf-8"))
+                self.serie.write(bytes(str(message) + Imprimante.newLine,"utf-8"))
             except Exception as e:
-                print(e)
+                print("Exception levée lors de l'envoi de la trame : "+e)
                 return None
                 
             #chaque envoi est acquité par le destinataire, pour permettre d'émettre en continu sans flooder la série
@@ -103,10 +116,10 @@ class Imprimante:
                     
                     if acquittement == "":
                         #renvoi de la trame
-                        self.serie.write(bytes(str(message) + '\r',"utf-8"))
+                        self.serie.write(bytes(str(message) + Imprimante.newLine,"utf-8"))
                         
             except Exception as e:
-                print(e)
+                print("Exception levée lors de l'acquittement : "+e)
                 return None
                 
         #liste des réponses
@@ -124,6 +137,11 @@ class Imprimante:
 ###       TRAMES SPÉCIFIQUES À L'IMPRIMANTE           ###
 #########################################################
      
+    def _pap_aller_a(self, position):
+        self.communiquer(["go_pap",position*1000],0)
+        while not int(self.communiquer("acq?",1)[0]):
+            time.sleep(0.1)
+            
     def initialise(self):
         """
         Définit la position courante du carton comme 0,
@@ -131,34 +149,30 @@ class Imprimante:
         et recale le moteur pas à pas.
         """
         
-        self.communiquer(["set_y",0.0],0)
+        self.communiquer(["set_mot",0],0)
         self.communiquer("asserv_on",0)
-        self.communiquer("recal_x",0)
-        
-        #acquittement d'arrivée par l'imprimante (booléen dans le 1er élément de la liste)
-        while not int(self.communiquer("acq?",1)[0]):
-            time.sleep(0.1)
+        self.recalage_x()
             
     def recalage_x(self):
         """
         Recale le moteur pas à pas sur une butée pour palier aux glissements.
         """
         
-        self.communiquer("recal_x",0)
-        
-        #acquittement d'arrivée par l'imprimante (booléen dans le 1er élément de la liste)
-        while not int(self.communiquer("acq?",1)[0]):
-            time.sleep(0.1)
+        self._pap_aller_a(-20)
+        self.communiquer("reset_pap",0)
+        self._pap_aller_a(4.680)
+        self.communiquer("reset_pap",0)
             
     def poinconne(self, x, y):
         """
         x correspond à une position atteinte par le moteur pas à pas (position du poinçon)
         y correspond à une position atteinte par les rouleaux (position du carton)
         L'imprimante s'y déplace, poinçonne, et rend la main au programme.
+        Les coordonnées sont toutes deux en microns et envoyées avec un entier.
         """
         
         #envoi des consignes (n'attend pas de réponse)
-        self.communiquer(["aller_a",x,y],0)
+        self.communiquer(["aller_a", int(1000*x), int(1000*y)],0)
         
         #acquittement d'arrivée par l'imprimante (booléen dans le 1er élément de la liste)
         while not int(self.communiquer("acq?",1)[0]):
@@ -167,6 +181,7 @@ class Imprimante:
         #ordre de poinçonnage (l'attente se fait grâce à une trame renvoyée en fin de poinçonnage)
         self.communiquer("poinconne",1)
         
+    #TODO
     def lit_pistes(self):
         """
         Renvoit une liste des id des trous lus à la position courante.
@@ -183,9 +198,17 @@ class Imprimante:
         else:
             return []
     
-    def sortir_carton(self):
+    def debut_sortir_carton(self):
         """
         Evacue le carton de l'imprimante en fin d'impression.
         """
         
-        self.communiquer("sortir",0)
+        self.communiquer(["go_mot",999999999],0)
+        
+    def fin_sortir_carton(self):
+        """
+        Stoppe le moteur lorsque l'utilisateur a vérifié la sortie du carton.
+        """
+        
+        self.communiquer(["set_mot",0],0)
+        self.communiquer("asserv_off",0)
